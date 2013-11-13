25f94fb875d84d56886c206d5ae08905277ed789
JMurphs <jamesus95@gmail.com>
2013-11-13 15:37:04 -0800

    Added dynamic functionality to event_dyn page using cookies

--- js/event_creator.js
+++ js/event_creator.js
@@ -10,11 +10,25 @@ function writeEvents() {
 
 	for (var i = 0; i < events.length; i++) {
 		if (events[i].month == month) {
-			thisMonth.append("<div>" + '<a class="event" href="event_dyn.htm">' + events[i].title + "</a>" + events[i].descript + "</div>");
+			thisMonth.append("<div>" + '<a class="event" id="' + events[i].id + '"' + events[i].title + '" href="event_dyn.htm">' + events[i].title + "</a>" + events[i].descript + "</div>");
 		} else if (events[i].month == month + 1) {
 			nextMonth.append("<div>" + events[i].descript + "</div>");
 			swNextMonth.append("<div>" + events[i].descript + "</div>");
 		}
 	}
 
+}
+
+function displayEvent() {
+	var id = document.cookie.split("=");
+	id = id[1];
+	var j = -1;
+	for (var i = 0; i < events.length; i++) {
+		if (events[i].id == id) {
+			j = i;
+		}
+	}
+	if (j > -1) {
+		$("#content_container").append("<div>" + events[j].title + "</div>");
+	}
 }
--- js/event_lib.js
+++ js/event_lib.js
@@ -1,15 +1,20 @@
-function makeEvent(monthi, dayi, timei, descriptioni, titlei) {
+function makeEvent(monthi, dayi, timei, descriptioni, titlei, idi) {
 	var e = {
 		month: monthi,
 		day: dayi,
 		startTime: timei,
 		descript: descriptioni,
-		title: titlei
+		title: titlei,
+		id: idi
 	}
 	return e;
 }
 
+function printLong(event) {
+	var s = "";
+}
+
 var events = new Array();
 
-events.push(makeEvent(11, 15, 1907,"Regular Meeting", "Friday UCF Meeting"));
+events.push(makeEvent(11, 15, 1907, "Regular Meeting", "Friday UCF Meeting", "11151907"));
 

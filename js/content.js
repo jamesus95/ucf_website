91ccd2966218b8aec29e1701ce16fb14beb6a25a
James Murphree <Jamesus95@gmail.com>
2013-11-13 20:42:42 -0800

    added htm: missions, leaders added css: Hi_res pic, js: added htm similar content injector

---
+++ js/content.js
@@ -0,0 +1,12 @@
+$("#nav_container").html('<nav><ul><li><a href="index.htm">Home</a></li><li><a href="events.htm">Events</a></li><li><a href="leaders.htm">Leaders</a></li><li><a href="missions.htm">Missions</a></li>');
+
+// $("#logo_container").html('<img src="../img/ucf_logo.jpg" id="logo">');
+$("#title_container").html('<h1>University Christian Fellowship UW</h1>');
+$("#sm-title_container").html('<h1>UCF UW</h1>');
+$("#important_container").html('<h2>IMPORTANT: Announcement</h2>');
+
+$("#about_container").html('<h2>What We\'re About:</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>');
+
+$("#sm-about_container").html('<h2>What We\'re About:</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,</p>');
+
+setTimeout('alert("Hello You\'ve been here 3 seconds")', 3000);
--- js/event_creator.js
+++ js/event_creator.js
@@ -1,22 +1,47 @@
 var date = new Date();
 var month = date.getMonth() + 1;
 var day = date.getDay() + 1;
+var year = date.getFullYear();
 
 var thisMonth = $("#thismonth_container");
 var nextMonth = $("#nextmonth_container");
 var swNextMonth = $("#sw-nextmonth_container");
 
-function writeEvents() {
+function makeEvent(monthi, dayi, yeari, timei, descriptioni, titlei, idi) {
+	var e = {
+		month: monthi,
+		day: dayi,
+		startTime: timei,
+		descript: descriptioni,
+		title: titlei,
+		id: idi,
+		year: yeari
+	}
+	return e;
+}
 
+function printLong(event) {
+	var s = event.title + "<br>" +
+	"Date: " + event.month + "/" + event.day + "/" + event.year + "<br>" +
+	"Time: " +  event.startTime + "<br>" +
+	event.descript;
+	return s;
+}
+
+var events = new Array();
+events.push(makeEvent(11, 15, 2013, "7:07pm", "Regular Meeting", "Friday UCF Meeting", "1115707"));
+events.push(makeEvent(11, 22, 2013, "7:07pm", "Regular Meeting 2", "Friday UCF Meeting 2", "1122707"))
+events.push(makeEvent(12, 10, 2013, "7:07pm", "Regular Meeting", "Friday UCF Meeting", "1210707"))
+
+function writeEvents() {
 	for (var i = 0; i < events.length; i++) {
 		if (events[i].month == month) {
-			thisMonth.append("<div>" + '<a class="event" id="' + events[i].id + '"' + events[i].title + '" href="event_dyn.htm">' + events[i].title + "</a>" + events[i].descript + "</div>");
+			thisMonth.append('<div><a class="event" id="' + events[i].id + '" href="event_dyn.htm">' + events[i].title + "</a> " + events[i].descript + "</div>");
 		} else if (events[i].month == month + 1) {
-			nextMonth.append("<div>" + events[i].descript + "</div>");
-			swNextMonth.append("<div>" + events[i].descript + "</div>");
+			nextMonth.append('<div><a class="event" id="' + events[i].id + '" href="event_dyn.htm">' + events[i].title + "</a> " + events[i].descript + "</div>");
+			swNextMonth.append('<div><a class="event" id="' + events[i].id + '" href="event_dyn.htm">' + events[i].title + "</a> " + events[i].descript + "</div>");
 		}
 	}
-
 }
 
 function displayEvent() {
@@ -34,6 +59,8 @@ function displayEvent() {
 		}
 	}
 	if (j > -1) {
-		$("#content_container").append("<div>" + events[j].title + "</div>");
+		$("#content_container").append(printLong(events[j]));
+		$("#map_dyn").html(events[j].title);
+		$("title").html(events[j].title);
 	}
 }

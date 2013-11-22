0931712d6560c553d9bfc5efb27e82d376111061
JMurphs <jamesus95@gmail.com>
2013-11-21 20:03:59 -0800

    switching to template load

---
+++ js/content_lib.js
@@ -0,0 +1,69 @@
+var breadCrumbs = "";
+
+var pageTitle = "";
+
+var content = "";
+
+var headerContainer = '<div class="col-md-9 hidden-sm hidden-xs" id="title-about_container"><div class="row" id="title_row"><div class="col-md-12" id="title_container"></div></div><div class="row" id="about_row"><div class="col-md-12" id="about_container"></div></div></div><div class="col-xs-8 hidden-md hidden-lg" id="sm-title_container"></div>';
+
+var smAboutRow = '<div class="row" id="sm-about_row"><div class="col-xs-12 hidden-md hidden-lg" id="sm-about_container"></div></div>';
+
+var mapRow = '<div class="row" id="map_row"><div class="col-xs-12 col-md-9 col-md-offset-3" id="map_container">' + breadCrumbs + '</div></div>';
+
+var pageTitleRow = '<div class="row" id="page-title_row"><div class="col-md-10 col-md-offset-2" id="page-title_container"><h2>' + pageTitle + '</h2></div></div>';
+
+var bodyRow = '<div class="row" id="body_row"><div class="col-md-2 hidden-sm hidden-xs" id="nav_container"></div><div class="col-md-2 hidden-md hidden-lg" id="nav-dropdown_container"></div><div class="col-md-10" id="body_container"><div class="row" id="important_row"><div class="col-md-12" id="important_container"></div></div><div class="row" id="content_row">' + content + '</div></div></div>';
+
+var titleContainer = '<h1>University Christian Fellowship UW</h1><p>Check us out on <a href="https://www.facebook.com/pages/University-Christian-Fellowship-UW/269814256707">Facebook</a></p>';
+
+var smTitleContainer = '<h1>UCF UW</h1>';
+
+var aboutContainer = '<div><h3>What We\'re About:</h3><p>UCF is a group of students at the University of Washington who desire to follow God with all we are.  We seek to be a place to belong, participate and transform.  We place high value on our friendships with each other and work hard to provide opportunities for them to both start and grow.  We welcome anyone to come and be part of UCF.  Whether you\'re just beginning to wonder about spirituality and who Jesus really is or have been following God for years, UCF is a place where you can ask questions, be challenged and grow by being a part of everything we are doing.  As each of us steps out to know Jesus better or to simply help those around us, we both take part in transforming the world for the better, and we experience God transforming each of us. <a href="' + getAboutLinkPrefix() + 'about.htm">click to learn more...</a></p></div>';
+
+var smAboutContainer = '<div><h3>What We\'re About:</h3><p>UCF is a group of students at the University of Washington who desire to follow God with all we are.  We seek to be a place to belong, participate and transform.  We place high value on our friendships with each other and work hard to provide opportunities for them to both start and grow.  We welcome anyone to come and be part of UCF. <a href="' + getAboutLinkPrefix() + 'about.htm">click to read more...</a></p></div>';
+
+var links = ["Home", "index.htm", "Activities", "activities.htm", "Teams", "teams.htm", "Service Activities", "service.htm", "Core Study Groups", "cores.htm", "Contact Information", "contact.htm", "About Us", "about.htm"];
+
+var css = ["libs/bootstrap-theme.min.css", "libs/bootstrap.min.css", "css/style.css", "css/layout.css", "css/state.css"];
+
+var important = "IMPORTANT: SITE UNDER CONSTRUCTION";
+
+var navDropdownContainer = '<div class="dropdown"><a id="dropdown_link" class="bold" data-toggle="dropdown" href="#">Links</a><ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">' + appendNav() + '</ul></div>';
+
+var navContainer = '<nav><ul>' + appendNav() + '</nav></ul>';
+
+function getAboutLinkPrefix() {
+	if ($("title").attr("id") == "index") {
+		return "htm/";
+	} else {
+		return "";
+	}
+}
+
+function getNavPrefix(i) {
+	if ($("title").attr("id") == "index") {
+		if (i == 0) {
+			return "";
+		} else {
+			return "htm/";
+		}
+	} else {
+		if (i == 0) {
+			return "../";
+		} else {
+			return "";
+		}
+	}
+}
+
+function appendNav() {
+	var s = "";
+	for (var i = 0; i < links.length; i += 2) {
+		s += link(links[i], links[i + 1], getNavPrefix(i));
+	}
+	return s;
+}
+
+function link(name, url, pre) {
+	return '<li><a href="' + pre + url + '">' + name + '</a></li>';
+}
--- js/load.js
+++ js/load.js
@@ -1,34 +1,3 @@
-// Creates the nav list elements
-function link(name, url, pre) {
-	return '<li><a href="' + pre + url + '">' + name + '</a></li>';
-}
-
-// Appends the nav list elements
-function appendNav(index, other) {
-	var s = "<nav><ul>";
-	for (var i = 0; i < tabs.length; i += 2) {
-		if (i == 0) {
-			s += link(tabs[i], tabs[i + 1], index);
-		} else {
-			s += link(tabs[i], tabs[i + 1], other);
-		}
-	}
-	s += "</ul></nav>";
-	$("#nav_container").html(s);
-}
-
-function appendNavDropdown(index, other) {
-	var s = "";
-	for (var i = 0; i < tabs.length; i += 2) {
-		if (i == 0) {
-			s += link(tabs[i], tabs[i + 1], index);
-		} else {
-			s += link(tabs[i], tabs[i + 1], other);
-		}
-	}
-	return s;
-}
-
 // Appends CSS documents
 function appendCSS(index) {
 	for (var i = 0; i < css.length; i++) {
@@ -36,66 +5,50 @@ function appendCSS(index) {
 	}
 }
 
-// hide till loaded
-function hide() {
-	$("body").css("display", "none");
-}
-
-// put in more about link
-function appendAboutLink() {
-	if ($("title").attr("id") == "index") {
-		return "htm/";
-	} else {
-		return "";
-	}
-}
-
-function load() {
-	// Title and about sections
-	$("#title_container").html('<h1>University Christian Fellowship UW</h1><p>Check us out on <a href="https://www.facebook.com/pages/University-Christian-Fellowship-UW/269814256707">Facebook</a></p>');
-	$("#sm-title_container").html('<h1>UCF UW</h1>');
-	$("#about_container").html('<div><h3>What We\'re About:</h3><p>UCF is a group of students at the University of Washington who desire to follow God with all we are.  We seek to be a place to belong, participate and transform.  We place high value on our friendships with each other and work hard to provide opportunities for them to both start and grow.  We welcome anyone to come and be part of UCF.  Whether you\'re just beginning to wonder about spirituality and who Jesus really is or have been following God for years, UCF is a place where you can ask questions, be challenged and grow by being a part of everything we are doing.  As each of us steps out to know Jesus better or to simply help those around us, we both take part in transforming the world for the better, and we experience God transforming each of us. <a href="' + appendAboutLink() + 'about.htm">click to learn more...</a></p></div>');
-	$("#sm-about_container").html('<div><h3>What We\'re About:</h3><p>UCF is a group of students at the University of Washington who desire to follow God with all we are.  We seek to be a place to belong, participate and transform.  We place high value on our friendships with each other and work hard to provide opportunities for them to both start and grow.  We welcome anyone to come and be part of UCF. <a href="htm/about.htm">click to read more...</a></p></div>');
-	if ($("title").attr("id") != "index") {
-		appendNav("../", "");
-		$("#nav-dropdown_container").html('<div class="dropdown"><a id="dropdown_link" class="bold" data-toggle="dropdown" href="#">Links</a><ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">' + appendNavDropdown("../", "") + '</ul></div>');
-	} else {
-		appendNav("", "htm/");
-		$("#nav-dropdown_container").html('<div class="dropdown"><a id="dropdown_link" class="bold" data-toggle="dropdown" href="#">Links</a><ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">' + appendNavDropdown("", "htm/") + '</ul></div>');
-	}
-	$("#important_container").html('<h3>' + imp + '</h3>');
-	$(window).ready(function() {
-		var height = $("#header_container").height();
-		var logoHeight = $("#logo_container").height();
-		var x = (height - logoHeight) / 2;
-		$("#logo_container").css("margin-top", x + "px");
-		$("body").css("display", "inline");
-		if ($(".container").height() < $(window).height()) {
-			$(".container").height($(window).height());
-		}
-	});
-}
-
-// Array of navs
-var tabs = ["Home", "index.htm", "Activities", "activities.htm", "Teams", "teams.htm", "Service Activities", "service.htm", "Core Study Groups", "cores.htm", "Contact Information", "contact.htm", "About Us", "about.htm"];
-
-// Array of CSS documents
-var css = ["libs/bootstrap-theme.min.css", "libs/bootstrap.min.css", "css/style.css", "css/layout.css", "css/state.css"];
-
-// Important Announcement
-var imp = "IMPORTANT: SITE UNDER CONSTRUCTION";
-
-// Resize function for logo
-$(window).resize(function() {
-	var height = $("#header_container").height();
-	var logoHeight = $("#logo_container").height();
-	var x = (height - logoHeight) / 2;
-	$("#logo_container").css("margin-top", x + "px");
-});
-
-// MAIN
 if ($("title").attr("id") != "index") {
 	appendCSS("../");
 } else {
 	appendCSS("");
 }
+
+// called at end of html
+function load() {
+
+	// Appending HTML
+	var container = $(".container");
+	var header = $("#header").html();
+	container.html("");
+	container.append(header);
+	$("#header_container").append(headerContainer);
+	container.append(smAboutRow, mapRow, pageTitleRow, bodyRow);
+	$("#title_container").html(titleContainer);
+	$("#sm-title_container").html(smTitleContainer);
+	$("#about_container").html(aboutContainer);
+	$("#sm-about_container").html(smAboutContainer);
+	$("#important_container").html(important);
+	$("#nav-dropdown_container").html(navDropdownContainer);
+	$("#nav_container").html(navContainer);
+
+	// Initial logo adjust and background extention
+	$(window).ready(function() {
+		var height = $("#header_container").height();
+		var logoHeight = $("#logo_container").height();
+		var x = (height - logoHeight) / 2;
+		$("#logo_container").css("margin-top", x + "px");
+		if ($(".container").height() < $(window).height()) {
+			$(".container").height($(window).height());
+		}
+	});
+
+	// Resize logo adjust
+	$(window).resize(function() {
+		var height = $("#header_container").height();
+		var logoHeight = $("#logo_container").height();
+		var x = (height - logoHeight) / 2;
+		$("#logo_container").css("margin-top", x + "px");
+	});
+}
+
+
+
+
